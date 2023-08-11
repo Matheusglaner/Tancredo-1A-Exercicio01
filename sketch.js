7c28434fcf9576c53cb53676064b834ec7b35588
glaner <141414389+Matheusglaner@users.noreply.github.com>
2023-08-11 08:18:27 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+background("blue");
+}
+
+function draw() {
+  
+
+  stroke("purple");
+  fill("gold");
+  
+  if(mouseIsPressed){rect (mouseX,mouseY,50,30);}
+    
+  }
+
